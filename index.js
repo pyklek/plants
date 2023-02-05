2f8fae8e7a79c941129d2926252741a0ba150eea
pyklek <vikadem03@gmail.com>
2023-02-05 22:06:27 +0300

    feat: add js

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ $(document).ready(function() {
 $("body").live('click', function() {
     alert($(this).text());
   });
+  const openPrices = document.querySelectorAll('.text_prices');
+  
+  openPrices.forEach((el) => el.firstElementChild.classList.add('no-border'));
+  
+  
+  for (let i=0; i<openPrices.length; i++) {
+     openPrices[i].addEventListener('click', (event) => {
+        if (event.target.classList.contains('') && openPrices[i].classList.contains('hide-open')) {
+           openPrices[i].classList.remove('hide-open');
+           openPrices[i].firstElementChild.classList.add('no-border');
+        } else {
+           openPrices.forEach((el) => el.classList.remove('hide-open'));
+           openPrices.forEach((el) => el.firstElementChild.classList.add('no-border'));
+           openPrices[i].classList.add('hide-open');
+           openPrices[i].firstElementChild.classList.remove('no-border');
+        }
+     })
+  }
